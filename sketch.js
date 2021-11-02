d87c0a013386ed07234bcc2dc22a109eaef70e44
Luis Molina-Tanco <lmtanco@users.noreply.github.com>
2021-11-02 20:19:33 +0100

    Ficheros del ejemplo globos 001
    Globos 001 es un paso intermedio en el ejercicio de objetos de Creativa. Esta es la versión p5.js

---
+++ sketch.js
@@ -0,0 +1,17 @@
+let globos = []; // variables en js. Arrays en js.  
+function setup() {
+  createCanvas(800, 800);
+}
+
+function draw() {
+  background(220);
+  for (let i=0; i<globos.length; i++) {
+    globos[i].update();
+    globos[i].show();
+  }
+}
+
+function mousePressed() {
+  globos.push(new Globo(mouseX,mouseY)); 
+}
+       
